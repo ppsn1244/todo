a68964b0906c48eee9f6d00f4f282602d7f6e5b0
ppsn1244 <96180085+ppsn1244@users.noreply.github.com>
2022-02-09 03:28:49 +0700

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    
+    <!-- #4 เรียกใช้ component ตาม tag ที่ประกาศไว้ -->
+    <todo-app>,</todo-app>
+    
+    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import todoApp from './components/todoApp.vue'
+//#2 import component
+
+
+export default {
+  name: 'App',
+  components: {
+    todoApp
+    //#3 register component/ ประกาศตัวแปร tag <...>
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
